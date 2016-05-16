306ced070a8321554ac89391a9c13c8b626c95f4
David Mogrovejo <dm228@njit.edu>
2016-05-16 12:26:52 -0400

    Added class IntegerGenerator

---
+++ PairsThatAddUpTo10/PairsThatAddUpTo10/IntegerGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairsThatAddUpTo10
+{
+    class IntegerGenerator
+    {
+        
+        /// <summary>
+        /// Creates an array of random integers
+        /// </summary>
+        /// <param name="sizeOfArray">Size of array</param>
+        /// <param name="min">Inclusive minumum integer value</param>
+        /// <param name="max">Exlusive maximum integer value</param>
+        /// <returns></returns>
+        public static int[] GetInteger(int sizeOfArray, int min, int max)
+        {
+
+            Random random = new Random();
+            int[] result = new int[sizeOfArray];
+            for(int i = 0; i < sizeOfArray; i++)
+            {
+
+                result[i] = random.Next(min, max);
+            }
+
+            return result;
+        }
+
+
+    }
+}
